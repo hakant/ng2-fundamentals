b32809bfc3cafe2a199ca6c8b00e17dee2edbc30
hakant <hakantuncer@gmail.com>
2017-04-16 15:58:28 +0200

    End of Chapter 6.10

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { EventService } from './shared/event.service';
 
 @Component({
-    selector: 'events-list',
-    templateUrl: 'app/events/events-list.component.html'
+  selector: 'events-list',
+  templateUrl: 'app/events/events-list.component.html'
 })
 export class EventsListComponent implements OnInit {
-  events: any[];
+  events: any;
 
-  constructor(private eventService: EventService) {
-    
-  }
-  
+  constructor(private eventService: EventService, private route: ActivatedRoute) { }
+
   ngOnInit() {
-    this.events = this.eventService.getEvents();
+    // this.eventService.getEvents().subscribe(events => {
+    //   this.events = events;
+    // });
+
+    this.events = this.route.snapshot.data['events'];
+
   }
 }
